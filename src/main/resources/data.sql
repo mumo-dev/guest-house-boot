c61b682ac2e85acf9fd6b3530ac7575015fc41d4
Samuel Mumo <samumo.dev@gmail.com>
2020-01-22 12:31:54 +0300

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,13 +5,11 @@ VALUES (1, 'ClientAddressType', now(), now());
 INSERT INTO address_types(id, name, created_at, updated_at)
 VALUES (2, 'GuestHouseAddressType', now(), now());
 
--- insert all counties
+--INSERT INTO users(id, first_name,last_name, email, password, active, created_at, updated_at)
+--VALUES (1, 'Samuel', 'Mumo','samuel@gmail.com','$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu',1, now(), now())
 
-
-INSERT INTO users(id, first_name,last_name, email, password, active, created_at, updated_at) VALUES (1, 'Samuel', 'Mumo','samuel@gmail.com','$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu',1, now(), now())
-
-INSERT INTO roles(id, name) VALUES (1, 'ADMIN')
-INSERT INTO roles(id, name) VALUES (2, 'CLIENT')
-INSERT INTO roles(id, name) VALUES (3, 'CUSTOMER')
+INSERT INTO roles(id, name, created_at, updated_at) VALUES (1, 'ADMIN', now(), now());
+INSERT INTO roles(id, name, created_at, updated_at) VALUES (2, 'CLIENT', now(), now());
+INSERT INTO roles(id, name, created_at, updated_at) VALUES (3, 'CUSTOMER', now(), now());
 
 INSERT INTO users_roles(users_id, roles_id) VALUES (1, 1)

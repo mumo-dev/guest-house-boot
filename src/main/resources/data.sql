b2be4fc9a9405daec67c24eb3ee7bc3da79fd1b3
jmumo <jaymumo6@gmail.com>
2020-02-16 18:08:26 +0300

    updates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,15 +8,23 @@ INSERT INTO address_types(id, name, created_at, updated_at)
 VALUES (2, 'GuestHouseAddressType', current_timestamp, current_timestamp);
 
 INSERT INTO users(id, first_name,last_name, email, password, active, created_at, updated_at)
-VALUES (1, 'Samuel', 'Mumo','samuel@gmail.com','$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu',true, current_timestamp, current_timestamp);
+VALUES (1, 'Samuel', 'Mumo','samuel@gmail.com','$2a$10$Z65pHQUCOGWXBDjDNu/Xf.Db06XuACBO1S5FjeXfqDAHw2oDU5D/C',true, current_timestamp, current_timestamp);
+
+INSERT INTO users(id, first_name,last_name, email, password, active, created_at, updated_at)
+VALUES (2, 'Samuel', 'Mumo','owner@gmail.com','$2a$10$Z65pHQUCOGWXBDjDNu/Xf.Db06XuACBO1S5FjeXfqDAHw2oDU5D/C',true, current_timestamp, current_timestamp);
+
+INSERT INTO users(id, first_name,last_name, email, password, active, created_at, updated_at)
+VALUES (3, 'Samuel', 'Mumo','guest@gmail.com','$2a$10$Z65pHQUCOGWXBDjDNu/Xf.Db06XuACBO1S5FjeXfqDAHw2oDU5D/C',true, current_timestamp, current_timestamp);
 
 INSERT INTO roles(id, name, created_at, updated_at) VALUES (1, 'ADMIN', current_timestamp, current_timestamp);
-INSERT INTO roles(id, name, created_at, updated_at) VALUES (2, 'CLIENT', current_timestamp, current_timestamp);
-INSERT INTO roles(id, name, created_at, updated_at) VALUES (3, 'CUSTOMER',current_timestamp, current_timestamp);
+INSERT INTO roles(id, name, created_at, updated_at) VALUES (2, 'OWNER', current_timestamp, current_timestamp);
+INSERT INTO roles(id, name, created_at, updated_at) VALUES (3, 'GUEST',current_timestamp, current_timestamp);
 INSERT INTO roles(id, name, created_at, updated_at) VALUES (4, 'EMAIL_VALIDATED',current_timestamp, current_timestamp);
 
 
 INSERT INTO users_roles(users_id, roles_id) VALUES (1, 1);
+INSERT INTO users_roles(users_id, roles_id) VALUES (2, 2);
+INSERT INTO users_roles(users_id, roles_id) VALUES (3, 3);
 
 INSERT INTO counties(id, name, created_at, updated_at) VALUES (1, 'Mombasa', current_timestamp, current_timestamp);
 INSERT INTO counties(id, name, created_at, updated_at) VALUES (2, 'Nairobi', current_timestamp, current_timestamp);
